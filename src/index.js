772e43834384db8838f2c30a21bf8fb400c8c932
Guilherme Padilha <gs.padilha@gmail.com>
2023-10-03 10:41:01 -0300

    feat: adição da tabela de visualização

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+import draftlog from "draftlog";
+import chalk from "chalk";
+import chalkTable from "chalk-table";
+
 import database from "../../database.json" assert { type: "json" };
 
-console.log("database", database);
+draftlog(console).addLineListener(process.stdin);
+
+const options = {
+  leftPad: 2,
+  columns: [
+    { field: "id", name: chalk.cyan("ID") },
+    { field: "vehicles", name: chalk.magenta("Vehicles") },
+    { field: "kmTraveled", name: chalk.cyan("KM Traveled") },
+    { field: "from", name: chalk.cyan("From") },
+    { field: "to", name: chalk.cyan("To") },
+  ],
+};
+
+console.draft(chalkTable(options, database));
